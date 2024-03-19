7d19f93735d13f1eb9788204fe2e8eba29864bdb
Liron Eldar <liron.eldar@tikalk.com>
2024-03-19 08:04:17 +0000

    Fix auth refresh

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { faker } from '@faker-js/faker'
-import { userCookieKey, cookieSep, createEncrypt } from 'libs/session'
+import { userCookieKey, cookieSep } from 'libs/session'
 import { sleep } from "libs/utils";
 
 export default async function middleware(req: NextRequest) {
